b0a2c1580a121ed916ec010918e5a8a6ab193762
CeeJay_Original <emmanuelcudjoe16@yahoo.com>
2023-05-22 23:39:01 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("CHANNEL_ID", "xxxxx")
+	os.Setenv("SLACK_BOT_TOKEN", "xxxxxxx")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"slack.txt"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+
+		file, err := api.UploadFile(params)
+
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		fmt.Printf("Name %s, URL:%s\n", file.Name, file.URLPrivate)
+	}
+}
